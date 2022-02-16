41eeb5c68a89dfcee566416b07ff95d2f860f740
Nícolas Lopes Redieske <89426735+nicolaslopess@users.noreply.github.com>
2022-02-16 02:09:01 -0300

    Update Valores.java

--- DesafioKoppEx1/src/desafiokoppex1/Valores.java
+++ DesafioKoppEx1/src/desafiokoppex1/Valores.java
@@ -28,39 +28,4 @@ public class Valores {
             soma += lista.get(i);
         }
     }
-
-
-    /*double[] n = new double[10];
-    int[] contador = new int[n.length];
-
-    void ordemValorManual() {
-        for (int i = 0; i < 10; i++) {
-            
-            System.out.print("Incira um valor: ");
-            Scanner sc = new Scanner(System.in);
-            double s = sc.nextDouble();
-            n[i] = s;
-            System.out.println("con: " + contador[i] + " n : " + n[i]);
-        }
-    }
-
-    void ordemValorFixo() {
-        double[] nf = {88.00, 130.00, 54.90, 293.30, 44.80};
-        for (int i = 0; i < nf.length; i++) {
-            System.out.println("valor fixo: " + nf[i]);
-        }
-
-    }
-
-    void soma() {
-        double vSoma = 0;
-        for (int i = 0; i < n.length; i++) {
-            vSoma = vSoma + n[i];
-        }
-        System.out.println("Total: " + vSoma);
-    }
-
-    void junsao() {
-
-    }*/
 }
